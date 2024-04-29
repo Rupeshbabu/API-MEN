3bc418c0edbd179b44f4648345f1c37be3b489e3
devopsrupesh <devopsrupesh2021@gmail.com>
2024-04-29 15:56:57 +0530

    factory

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,15 +1,23 @@
 const catchAsync = require("../utils/catchAsync");
 
-exports.deleteOne = Model => catchAsync (async (req, res, next) =>{
-    const doc = await Model.findByIdAndDelete(req.params.id);
-      if(!doc){
-        return res.status(404).json({
-          status: 'fail',
-          message: 'No document found with that ID'
-        });
-      }
-      res.status(204).json({
-        status: 'success',
-        data: null,
-      });
+exports.deleteOne = Model => catchAsync(async (req, res, next) => {
+  const doc = await Model.findByIdAndDelete(req.params.id);
+  if (!doc) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'No document found with that ID'
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+exports.updateOne = Model => catchAsync(async (req, res, next) => {
+
+});
+
+exports.createOne = Model => catchAsync(async (req, res, next) => {
+
 });
